3568258c2c735f609ee44fe79561bf9cb39865bc
JohnAntz <johnantzoulisvazquez@outlook.com>
2025-02-06 14:04:35 +0100

    Create 4 basic functions for mathematical operations.

---
+++ main.js
@@ -0,0 +1,19 @@
+const add = function (a, b) {
+  const result = a + b;
+  return result;
+};
+
+const subtract = function (a, b) {
+  const result = a - b;
+  return result;
+};
+
+const multiply = function (a, b) {
+  const result = a * b;
+  return result;
+};
+
+const divide = function (a, b) {
+  const result = a / b;
+  return result;
+};
